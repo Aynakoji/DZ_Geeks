70556eab1df3703a014c92c14aa8ffc591d14cc2
Aynakoji <132373539+Aynakoji@users.noreply.github.com>
2023-05-10 01:31:07 +0600

    Выполнил DZ-2

---
+++ DZ2- Ibragim -JS/index.js
@@ -0,0 +1,80 @@
+//ЗАДАЧА № 1!!!!
+
+// var arr = [10, 20, 30, 50, 235, 3000, 5000439857]
+
+// for (var i = 0; i < arr.length; i++) {
+//     var num = String(arr[i])
+//     var char = num[0]
+//     if (i === 0 || i === 1 || i === 3 || i === 4 || i === 6) {
+//         console.log(arr[i])
+//     }
+
+// }
+
+// ЗАДАЧА № 2!!!!
+
+// var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] 
+//     console.log(num.reverse());
+
+
+
+// ЗАДАЧА № 3!!!!!!
+
+// var input = prompt("Введите цвет светафора")
+// var traffic_light = {
+//     red:"red",
+//     yellow: "yellow",
+//     green:"green"
+// }
+
+// if( input == traffic_light.red){
+//     alert('STOP!')
+// }else if(input == traffic_light.yellow){
+//     alert("get ready!")
+// }else if(input == traffic_light.green){
+//     alert("GO!")
+// }else{
+//     alert("I said the colors of the lights!")
+// }
+
+// switch(input) {
+//     case traffic_light.red:
+//         alert('STOP!')
+//         break
+//     case traffic_light.yellow:
+//         alert("get ready!")
+//         break
+//     case traffic_light.green:
+//         alert("GO!")
+//         break
+//     default:
+//         alert("I said the colors of the lights!")
+//         break
+// }
+
+
+
+
+
+
+
+  // ЗАДАЧА № 4!!!
+
+
+
+
+
+
+var arr = []
+
+
+
+for (arr = 0; arr < 10; arr++) {
+    // arr.push(10)
+    console.log(arr);
+}
+console.log(arr);
+
+
+
+
