1f1a93dda5e14621d19a3aa1449c489bda2a2311
Aynakoji <132373539+Aynakoji@users.noreply.github.com>
2023-05-17 04:58:54 +0600

    DZ4
    было очень сложно

--- DZ4_Ibragim-Js/index.js
+++ DZ4_Ibragim-Js/index.js
@@ -1,37 +1,37 @@
 // ЗАДАЧА № 1!!!
 
 
-var decrementButton = document.getElementById('decrement')
-var incrementButton = document.getElementById('increment')
-var counterElement = document.getElementById('counter')
+// var decrementButton = document.getElementById('decrement')
+// var incrementButton = document.getElementById('increment')
+// var counterElement = document.getElementById('counter')
 
-var counterValue = 1
-counterElement.textContent = counterValue
+// var counterValue = 0
+// counterElement.textContent = counterValue
 
-function updateCounter(value) {
-  counterValue += value
-  counterElement.textContent = counterValue
+// function updateCounter(value) {
+//   counterValue += value
+//   counterElement.textContent = counterValue
 
-  if (counterValue > 0) {
-    counterElement.add('increment')
-    counterElement.remove('decrement')
-  } else if (counterValue < 0) {
-    counterElement.add('decrement')
-    counterElement.remove('increment')
-  } else {
-    counterElement.remove('decrement', 'increment')
-  }
-}
+//   if (counterValue > 0) {
+//     counterElement.add('increment')
+//     counterElement.remove('decrement')
+//   } else if (counterValue < 0) {
+//     counterElement.add('decrement')
+//     counterElement.remove('increment')
+//   } else {
+//     counterElement.remove('decrement', 'increment')
+//   }
+// }
 
-decrementButton.addEventListener('click', function() {
-  if (counterValue > 0) {
-    updateCounter(-1)
-  }
-});
+// decrementButton.addEventListener('click', function() {
+//   if (counterValue > 0) {
+//     updateCounter(-1)
+//   }
+// });
 
-incrementButton.addEventListener('click', function() {
-  updateCounter(1)
-})
+// incrementButton.addEventListener('click', function() {
+//   updateCounter(1)
+// })
 
 
 
@@ -39,28 +39,93 @@ incrementButton.addEventListener('click', function() {
 // ЗАДАЧА № 2!!!!
 
 
-const myButton = document.getElementById('myButton');
-const outputElement = document.getElementById('output');
+// const myButton = document.getElementById('myButton');
+// const outputElement = document.getElementById('output');
 
-myButton.addEventListener('click', function() {
-  const userInput = prompt('');
-  outputElement.textContent = userInput;
-});
+// myButton.addEventListener('click', function() {
+//   const userInput = prompt('');
+//   outputElement.textContent = userInput;
+// });
 
 
 // ЗАДАЧА № 3!
 
+//IF ELSE
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const lights = document.getElementsByClassName("light");
+//   lights[0].classList.add("active");
+
+//   function setLight(color) {
+//     color = color.toLowerCase();
+  
+//     for (let i = 0; i < lights.length; i++) {
+//       lights[i].classList.remove("active");
+//     }
+  
+//     if (color === "красный" || color === "red") {
+//       lights[0].classList.add("active");
+//       console.log("STOP");
+//     } else if (color === "желтый" || color === "yellow") {
+//       lights[1].classList.add("active");
+//       console.log("WAIT");
+//     } else if (color === "зеленый" || color === "green") {
+//       lights[2].classList.add("active");
+//       console.log("GO");
+//     } else {
+//       console.log("Вводи цвета!");
+//     }
+//   }
+  
+//   const inputColor = prompt("Введите цвет :");
+//   setLight(inputColor);
+// });
 
 
 
-const userInput = prompt('Введите цвет светофора):');
-const trafficLight = document.querySelector('.traffic-light');
-const lowercaseInput = userInput.toLowerCase();
 
-if (lowercaseInput === 'красный') {
-  trafficLight.querySelector('.red').classList.add('active');
-  alert('STOP');
-} else if (lowercaseInput === 'зеленый') {
-  trafficLight.querySelector('.green').classList.add('active');
-  alert('GO');
-}
+
+
+
+
+
+
+
+// SWITCH CASE
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const lights = document.getElementsByClassName("light");
+//   lights[0].classList.add("active");
+
+//   function setLight(color) {
+//     color = color.toLowerCase();
+  
+//     for (let i = 0; i < lights.length; i++) {
+//       lights[i].classList.remove("active");
+//     }
+  
+//     switch (color) {
+//       case "красный":
+//       case "red":
+//         lights[0].classList.add("active");
+//         console.log("STOP");
+//         break;
+//       case "желтый":
+//       case "yellow":
+//         lights[1].classList.add("active");
+//         console.log("WAIT");
+//         break;
+//       case "зеленый":
+//       case "green":
+//         lights[2].classList.add("active");
+//         console.log("GO");
+//         break;
+//       default:
+//         console.log("Вводи цвета!");
+//     }
+//   }
+  
+  // const inputColor = prompt("Введите цвет :");
+//   setLight(inputColor);
+// });
