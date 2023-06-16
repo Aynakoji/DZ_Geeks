be87ea9071cf857e9044ee74a774a17cefbdd64b
Aynakoji <132373539+Aynakoji@users.noreply.github.com>
2023-06-17 05:20:56 +0600

    дз 3
    дз 3

--- project-GEEKS/js/homeworks.js
+++ project-GEEKS/js/homeworks.js
@@ -81,3 +81,46 @@ let counterValue = 0;
     stopBtn.addEventListener("click", stopCounter);
     resumeBtn.addEventListener("click", resumeCounter);
     clearBtn.addEventListener("click", clearCounter);
+
+
+
+
+// MODAL
+
+
+
+
+
+const modal = document.querySelector('.modal');
+const modalTrigger = document.querySelector('#btn-get');
+const closeModalButton = document.querySelector('.modal_close');
+
+const openModal = () => {
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+};
+
+const closeModal = () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
+};
+
+modalTrigger.onclick = () => openModal();
+closeModalButton.onclick = () => closeModal();
+modal.onclick = (event) => event.target === modal && closeModal();
+
+const openModalEvery10Seconds = () => {
+    setInterval(openModal, 10000); // Open modal every 10 seconds
+};
+
+const checkScrollBottom = () => {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const bodyHeight = document.documentElement.offsetHeight;
+    if (scrollPosition >= bodyHeight) {
+        openModal();
+    }
+};
+
+window.addEventListener('scroll', checkScrollBottom);
+
+openModalEvery10Seconds();
--- project-GEEKS/js/main.js
+++ project-GEEKS/js/main.js
@@ -56,3 +56,42 @@ const autoSlider = (i = 0) => {
     }, 6000)
 }
 autoSlider()
+
+
+
+
+
+
+const modal = document.querySelector('.modal');
+const modalTrigger = document.querySelector('#btn-get');
+const closeModalButton = document.querySelector('.modal_close');
+
+const openModal = () => {
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+};
+
+const closeModal = () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
+};
+
+modalTrigger.onclick = () => openModal();
+closeModalButton.onclick = () => closeModal();
+modal.onclick = (event) => event.target === modal && closeModal();
+
+const openModalEvery10Seconds = () => {
+    setInterval(openModal, 10000); // Open modal every 10 seconds
+};
+
+const checkScrollBottom = () => {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const bodyHeight = document.documentElement.offsetHeight;
+    if (scrollPosition >= bodyHeight) {
+        openModal();
+    }
+};
+
+window.addEventListener('scroll', checkScrollBottom);
+
+openModalEvery10Seconds();
